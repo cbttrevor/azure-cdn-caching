524170e445500bc313b68ef52e6cefc786356f24
Trevor Sullivan <trevor@trevorsullivan.net>
2020-09-02 11:53:10 -0600

    Update project files 🚀

--- 06-Use Azure PowerShell Module to Create a Redis Cache/01-create-resource-group.ps1
+++ 06-Use Azure PowerShell Module to Create a Redis Cache/01-create-resource-group.ps1
@@ -1,2 +1,5 @@
-$Location = 
-New-AzResourceGroup 
+# Create a new Azure Resource Group to contain the Redis Cache
+
+$Location = 'West US 2'
+
+New-AzResourceGroup -Name redis -Location $Location
--- 06-Use Azure PowerShell Module to Create a Redis Cache/02-create-redis-cache.ps1
+++ 06-Use Azure PowerShell Module to Create a Redis Cache/02-create-redis-cache.ps1
@@ -1,3 +1,3 @@
 # Create an Azure Redis Cache
-# NOTE: The SKU should be specified, along with the Size parameters, otherwise you'll get a default 1GB cache
+# NOTE: The SKU should be specified, along with the Size parameters, otherwise you'll get a Standard 1GB cache
 New-AzRedisCache -Name trevor2 -ResourceGroupName redis -Location 'West US 2' -Size 250MB -MinimumTlsVersion 1.2
